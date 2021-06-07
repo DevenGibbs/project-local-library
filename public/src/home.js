4fdf81a414740e3cafbe5a309ac166e3377720a4
Deven Gibbs <devengibbs91@gmail.com>
2021-06-07 10:34:24 -0500

    Complete qualified assignment, ...

---
+++ public/src/home.js
@@ -0,0 +1,83 @@
+function getTotalBooksCount(books) {
+  return books.length;
+}
+
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
+
+function getBooksBorrowedCount(books) {
+  return books.reduce((acc, book) => book.borrows[0].returned ? acc : ++acc, 0);
+}
+
+function getMostCommonGenres(books) {
+  const genresArray = [];
+  books.forEach((book) => {
+    if (genresArray.some((gen) => gen.name === book.genre)) {
+      for (let i = 0; i < genresArray.length; i++) {
+        if (genresArray[i].name === book.genre) {
+          genresArray[i].count += 1
+        }
+      }
+    } else {
+      let newObject = {};
+      newObject.name = book.genre;
+      newObject.count = 1;
+      genresArray.push(newObject);
+    }
+  })
+  genresArray.sort((a, b) => b.count - a.count)
+  return genresArray.slice(0,5);
+}
+
+//Helper function
+function sortAndSliceResults(resultArray) {
+  resultArray.sort((a, b) => a.count < b.count ? 1 : -1);
+  return resultArray.slice(0,5);
+}
+
+function getMostPopularBooks(books) {
+  let result = books.map((book) => {
+    let newObject = {}
+    newObject.name = book.title;
+    newObject.count = book.borrows.length;
+    return newObject
+  })
+  return sortAndSliceResults(result);
+}
+
+function getMostPopularAuthors(books, authors) {
+  books.forEach((book) => {
+    let numberOfBorrows = book.borrows.length;
+    let theAuthor = authors.find((person) => person.id === book.authorId);
+    let authorName = `${theAuthor.name.first} ${theAuthor.name.last}`;
+    book["name"] = authorName;
+    book["count"] = numberOfBorrows
+  })
+  let newArray = books.map(({name, count}) => ({name, count}));
+  let finalArray = [];
+  newArray.forEach((item) => {
+    if(finalArray.some((obj) => obj.name === item.name)) {
+      for (let i = 0; i <finalArray.length; i++) {
+        if(finalArray[i].name === item.name) {
+          finalArray[i].count += item.count
+        }
+      }
+    } else {
+      let newObject = {};
+      newObject.name = item.name;
+      newObject.count = item.count;
+      finalArray.push(newObject);
+    }
+  })
+  return sortAndSliceResults(finalArray);
+}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
